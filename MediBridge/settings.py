d68de9a0b310b8361a48d36c2e7c591e1e9e4d8b
AnupBudhathoki <kshetria656@gmail.com>
2026-03-18 12:22:31 +0545

    connect postgresql database

--- MediBridge/settings.py
+++ MediBridge/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url # type: ignore
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,10 +67,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'MediBridge.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
